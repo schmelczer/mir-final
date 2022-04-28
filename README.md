196de157115d44c66418780c7fcb71b0afb7dcbe
András Schmelczer <andras@schmelczer.dev>
2022-04-28 08:41:11 +0200

    Add embeddings

---
+++ README.md
@@ -0,0 +1,12 @@
+## Sources
+
+### Birds
+
+- [CUB-200](https://www.kaggle.com/datasets/veeralakrishna/200-bird-species-with-11788-images?resource=download)
+- [Birds](https://drive.google.com/file/d/1O_LtUP9sch09QH3s_EBAgLEctBQ5JBSJ/view)
+
+### Flowers
+
+- [Oxford flowers 102](https://www.robots.ox.ac.uk/~vgg/data/flowers/102/)
+- [Flowers](https://drive.google.com/uc?export=download&confirm=l7Ld&id=0B0ywwgffWnLLcms2WWJQRFNSWXM)
+- [Flower names](https://www.kaggle.com/datasets/nunenuh/pytorch-challange-flower-dataset)
---
+++ execute-notebook.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]
+then
+    echo "Provide the path to a Jupyter notebook: $0 example.ipynb"
+    exit -1
+fi
+
+if ! command -v tmux -V &> /dev/null
+then
+    echo "tmux could not be found, installing..."
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install tmux
+    else
+        sudo apt install -y tmux
+    fi
+fi
+
+VENV_PATH=$(dirname "$0")
+VENV_PATH="$VENV_PATH/.env/bin/activate"
+
+TUTORIAL="Press CTRL+B D to detach from the session"
+SCRIPT=`python3.9 -m nbconvert --to script $1 --stdout`
+SESSION_NAME="$( LC_ALL=C eval printf '%s' "${1//[^a-zA-Z0-9]/_}" )"
+COMMAND="echo $TUTORIAL && source $VENV_PATH && (python3 - 2>&1 <<'EOF'"$'\n'"$SCRIPT"$'\nEOF\n) | tee '"$SESSION_NAME.log"
+
+tmux new-session -s "$SESSION_NAME" "$COMMAND"
